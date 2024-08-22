4f2f1b7a1ad2839fabfff899f7d28a5879c2e4f8
eunbinpark <534311@gmail.com>
2024-08-22 13:21:58 +0900

    fixed : 쿼리 수정 서버 테스트 중

--- src/main/java/io/cloudtype/waitlist/repository/DrugRepository.java
+++ src/main/java/io/cloudtype/waitlist/repository/DrugRepository.java
@@ -6,16 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface DrugRepository extends JpaRepository<Drug, Long> {
-    @Query(value = "SELECT new io.cloudtype.waitlist.dto.DrugDto(i.ingredient_eng AS ingredient, d.tkiddi_kor AS ddi, i.ingredient_efficacy AS efficacy) " +
+    @Query(value = "SELECT i.ingredient_eng as ingredient, d.tkiddi_kor as ddi, i.ingredient_efficacy efficacy " +
             "FROM Connect c " +
             "JOIN Drug_Drug_Interaction d ON c.tkiddi_id = d.tkiddi_id " +
             "JOIN TKI t ON c.tki_id = t.tki_id " +
             "JOIN Ingredient i ON c.ingredient_id = i.ingredient_id " +
             "JOIN Brandname b ON i.ingredient_id = b.ingredient_id " +
             "WHERE t.tki_eng = :tki " +
-            "AND b.brandname = :drug")
+            "AND b.brandname = :drug", nativeQuery = true)
     DrugDto searchDrug(@Param("tki") String tki, @Param("drug") String drug);
 }
