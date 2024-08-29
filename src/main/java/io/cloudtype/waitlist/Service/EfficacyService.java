420476a29a11910fd0109193e5ce085316a74bb1
eunbinpark <534311@gmail.com>
2024-08-29 11:29:05 +0900

    fixed : 대체약물 쿼리 수정

--- src/main/java/io/cloudtype/waitlist/Service/EfficacyService.java
+++ src/main/java/io/cloudtype/waitlist/Service/EfficacyService.java
@@ -1,10 +1,12 @@
 package io.cloudtype.waitlist.Service;
 
+import io.cloudtype.waitlist.dto.DrugDto;
 import io.cloudtype.waitlist.dto.EfficacyDto;
 import io.cloudtype.waitlist.mapper.EfficacyMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,6 +19,13 @@ public class EfficacyService {
     }
 
     public List<EfficacyDto> searchEfficacy(String tki, String efficacy) {
-        return efficacyMapper.searchEfficacy(tki, efficacy);
+        List<EfficacyDto> result = efficacyMapper.searchEfficacy(tki, efficacy);
+        if (result == null || result.isEmpty()) {
+            result = new ArrayList<>();
+            EfficacyDto efficacyDto = new EfficacyDto();
+            efficacyDto.setIngredient(null);
+            result.add(efficacyDto);
+        }
+        return result;
     }
 }
